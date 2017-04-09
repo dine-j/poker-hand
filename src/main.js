b92f192bc1010317292a1614c293929b865ed0d3
Amandine Jala <amandine.jala@kcl.ac.uk>
2017-04-09 21:13:55 +0400

    Fix main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var PokerHand = require('../src/pokerHand');
+var PokerHand = require('../src/pokerHand').PokerHand;
 var Result = require('../src/pokerHand').Result;
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -10,30 +10,27 @@ console.log("This program compares 2 poker hands and determines a winner");
 
 var player1 = "";
 var player2= "";
-var hand1 = "";
-var hand2 = "";
-
 rl.question('Player 1 name: ', function(answer) {
     player1 = answer;
 
     rl.question('Please type in your cards: ', function (answer) {
-        hand1 = new PokerHand(answer);
+        var hand1 = new PokerHand(answer);
 
         rl.question('Player 2 name: ', function (answer) {
             player2 = answer;
 
             rl.question('Please type in your cards: ', function (answer) {
-                hand2 = new PokerHand(answer);
+                var hand2 = new PokerHand(answer);
 
                 rl.close();
+
+                if(hand1.compareWith(hand2) === Result.win)
+                    console.log(player1 + " wins!");
+                else if(hand1.compareWith(hand2) === Result.loss)
+                    console.log(player2 + " wins!");
+                else
+                    console.log("It's a tie!");
             });
         });
     });
 });
-
-if(hand1.compareWith(hand2) === Result.win)
-    console.log(player1 + " wins!");
-else if(hand1.compareWith(hand2) === Result.loss)
-    console.log(player2 + " wins!");
-else
-    console.log("It's a tie!");
